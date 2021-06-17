f6c65e68c5e8b7865022c596c6f7eded600d6058
InteOskar <oskarwennerlund@hotmail.com>
2021-06-17 13:45:17 +0200

    Update css/js

--- js/budget/createBudgetForm.js
+++ js/budget/createBudgetForm.js
@@ -1,22 +1,27 @@
 const createBudgetForm = (parent) => {
   const form = document.createElement("form");
+  form.className="createBudget-form";
   const inputContainer = document.createElement("div");
   inputContainer.className = "input-container";
-
+  const addBtnDropDownContainer = document.createElement("div");
+  addBtnDropDownContainer.className = "buttons-container";
+  
   //for loop
-  for (let index = 0; index < 10; index++) {
+  for (let index = 0; index < 5; index++) {
     const inputDiv = document.createElement("div");
     createLabel(inputDiv);
     createInput(inputDiv);
-    createRadio(inputDiv);
+    createCheckbox(inputDiv);
+    inputDiv.className="input-content";
     inputContainer.appendChild(inputDiv);
   }
   form.appendChild(inputContainer);
-
+  
   //static
-  createDropdown(form);
-  createAddCategory(form);
-  createButtons(form);
+  createDropdown(addBtnDropDownContainer);
+  createAddCategory(addBtnDropDownContainer);
+  createButtons(addBtnDropDownContainer);
+  form.appendChild(addBtnDropDownContainer);
   parent.appendChild(form);
 };
 const createLabel = (form) => {
@@ -28,24 +33,33 @@ const createLabel = (form) => {
 const createInput = (form) => {
   const input = document.createElement("input");
   input.type = "number";
+  input.className = "amount-input";
   form.appendChild(input);
 };
-const createRadio = (form) => {
-  const radio = document.createElement("input");
-  radio.type = "radio";
-  form.appendChild(radio);
+const createCheckbox = (form) => {
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.className = "checkbox-input";
+  form.appendChild(checkbox);
 };
 
 const createButtons = (parent) => {
+
+  const btnDropdown = document.createElement("div")
+  btnDropdown.className = "btnDropdown-container"
+
   const container = document.createElement("div");
-  container.className = "add-budget-btn-container";
+  container.className = "clearSave-btn-container";
+  
 
   const clearBtn = document.createElement("button");
   const clearBtnText = document.createTextNode("Clear");
+  clearBtn.className ="clear-btn";
   clearBtn.appendChild(clearBtnText);
 
   const saveBtn = document.createElement("button");
   const saveBtnText = document.createTextNode("Save");
+  saveBtn.className ="save-btn";
   saveBtn.appendChild(saveBtnText);
 
   container.appendChild(clearBtn);
@@ -60,6 +74,7 @@ const createDropdown = (parent) => {
 const createAddCategory = (parent) => {
   const addCategoryBtn = document.createElement("button");
   const textNode = document.createTextNode("+");
+  addCategoryBtn.className = "addCategory-btn";
   addCategoryBtn.appendChild(textNode);
   parent.appendChild(addCategoryBtn);
 };
