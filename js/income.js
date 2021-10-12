192f2a25943960bdd219945628ec17f8f19920e0
Wadstrom <73141419+Wadstrom@users.noreply.github.com>
2021-10-12 09:56:57 +0200

    Städ samt bytt en h3 till p i homepage

--- js/income.js
+++ js/income.js
@@ -5,19 +5,15 @@ import {welcomeMessage} from "./homepage.js";
 
 forms.onsubmit = (e) => {
   e.preventDefault();
-  console.log(e);
-
-  let requestObject = {
+    let requestObject = {
     Name: e.target[0].value,
     Amount: e.target[1].value,
     Date: e.target[2].value,
     UserID: cookieUserID,
   };
-
   postByModel(requestObject, "Income");
 };
 
-//GET
 getDataByName("Income").then((data) => {
   data.forEach((obj) => {
     obj.Date = obj.Date.slice(0, 10);
