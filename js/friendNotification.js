ce89fae129e29086ce0584044327710f15c5454d
Erik <boneblooder@hotmail.com>
2021-11-04 13:32:56 +0100

    Implementerat men innte helt klart

--- js/friendNotification.js
+++ js/friendNotification.js
@@ -1,12 +1,11 @@
 import { getDataByName } from "./fetches.js"
 
-/* vi behöver hämta friend requests. (tror vi har en fetch för det)
-    efter det display notifaction box över friend i "menyn".
-    vi behöver även ta bort den efter att clienten sett/gått in på social sidan.
+/* vi behöver hämta friend requests. (tror vi har en fetch för det) --KLART
+    efter det display notifaction box över friend i "menyn". --KLART
+    vi behöver även ta bort den efter att clienten sett/gått in på social sidan. 
     
-    Metod som hämtar requests och sedan skapar divven för notification.
-    skapa div med notifaction box > event listener  som sakpar den? 
-    Kolla om vi behöver en ny fetch.¨¨¨
+    Metod som hämtar requests och sedan skapar divven för notification. --Måste implementeras
+    Kolla om vi behöver en ny fetch.¨¨¨  --NEJ --KLART
     
     EMAIL VALIDATION IN FRIEND BE. wtf vi har gjort det!!!
     */
@@ -21,12 +20,15 @@ const checkNotifacion = (list) => {
     list.forEach(element => {
         
         if (element.List_ID === 1) {
+            const div = document.createElement('div')
             const social = document.getElementsByClassName('social')
             const text=document.createElement('p')
             const textnode = document.createTextNode('!')
-            text.id="notification"
+            div.id="notificationDiv"
+            text.id="notificationText"
+            div.appendChild(text)
             text.appendChild(textnode)
-            social[0].append(text)
+            social[0].append(div)
         }
     });
 }
