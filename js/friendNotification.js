b424fc5f68a0549be81711eaae6e96702b96aeee
InteOskar <oskarwennerlund@hotmail.com>
2021-11-08 16:21:24 +0100

    Fixed Notification

--- js/friendNotification.js
+++ js/friendNotification.js
@@ -1,15 +1,5 @@
 import { getDataByName } from "./fetches.js"
 
-/* vi behöver hämta friend requests. (tror vi har en fetch för det) --KLART
-    efter det display notifaction box över friend i "menyn". --KLART
-    vi behöver även ta bort den efter att clienten sett/gått in på social sidan. 
-    
-    Metod som hämtar requests och sedan skapar divven för notification. --Måste implementeras
-    Kolla om vi behöver en ny fetch.¨¨¨  --NEJ --KLART
-    
-    EMAIL VALIDATION IN FRIEND BE. wtf vi har gjort det!!!
-    */
-
     export const friendRequests = async () => {
         const friendlist = getDataByName("Friend")
        checkNotifacion(await friendlist)
@@ -20,15 +10,18 @@ const checkNotifacion = (list) => {
     list.forEach(element => {
         
         if (element.List_ID === 1) {
-            const div = document.createElement('div')
+            const notifactionElement = document.getElementById('notificationDiv')
+            if(!notifactionElement){
             const social = document.getElementsByClassName('social')
+            const notificationDiv = document.createElement('div')
+            notificationDiv.id = 'notificationDiv'
             const text=document.createElement('p')
             const textnode = document.createTextNode('!')
-            div.id="notificationDiv"
-            text.id="notificationText"
-            div.appendChild(text)
+            text.id="notification"
             text.appendChild(textnode)
-            social[0].append(div)
+            notificationDiv.append(text)
+            social[0].append(notificationDiv)
+            }
         }
     });
 }
